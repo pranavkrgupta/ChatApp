3c819580dc5a6398a116bf6d37a876d6c0f03bcb
Pranav Kumar Gupta <pranavkumar2998@gmail.com>
2024-07-26 00:55:14 +0530

    Add accessChat functionality

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
 dotenv.config();
 const PORT = process.env.PORT || 4000;
 const { notFound, errorHandler } = require("./Middleware/errorMiddleware");
@@ -18,6 +19,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
